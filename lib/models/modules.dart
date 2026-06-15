07b3dca8f766721e7730e59c6e4cb44d62c38d25
chriswafula <162628908+chriswafula@users.noreply.github.com>
2026-06-15 22:58:14 +0300

    Create modules.dart

---
+++ lib/models/modules.dart
@@ -0,0 +1,13 @@
+class GermanModule {
+  final String id;
+  final String title;
+  final String description;
+  final int price;
+
+  GermanModule({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+  });
+}
